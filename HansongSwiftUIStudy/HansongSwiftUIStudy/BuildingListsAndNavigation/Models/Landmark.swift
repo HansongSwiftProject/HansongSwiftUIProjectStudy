9f38cea7345a43b57d77b4a1686324fcde2f32a4
hansong8675 <hansong8675@gmail.com>
2020-10-26 15:43:38 +0700

    本地json 数据解析  本地资源图片加载  单例模式

---
+++ HansongSwiftUIStudy/HansongSwiftUIStudy/BuildingListsAndNavigation/Models/Landmark.swift
@@ -0,0 +1,42 @@
+//
+//  Landmark.swift
+//  HansongSwiftUIStudy
+//
+//  Created by hansong on 10/26/20.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark:Hashable,Codable,Identifiable {
+    var id:Int
+    var name:String
+    fileprivate var imageName:String
+    fileprivate var coordinates:Coordinates
+    var state:String
+    var park:String
+    var category:Category
+    
+    var locationCoordinate:CLLocationCoordinate2D{
+        CLLocationCoordinate2D(latitude: coordinates.latitude, longitude: coordinates.longitude)
+    }
+    
+    enum Category:String,CaseIterable,Codable,Hashable {
+        case featured = "Featured"
+        case lakes = "Lakes"
+        case rivers = "rivers"
+    }
+
+}
+extension Landmark{
+    var image:Image{
+        ImageStore.shared.image(name:imageName)
+    }
+
+}
+
+struct Coordinates:Hashable,Codable {
+    var latitude:Double
+    var longitude:Double
+}
